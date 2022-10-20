58ae0d4b27b8443e51a6542b3b8be69a044a5272
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-20 11:28:44 +0600

    first commit

---
+++ error.js
@@ -0,0 +1,41 @@
+// 1. syntax error
+
+// console.log(4;
+//  function add+(){
+
+//  }
+
+// Let x = 12;
+// cont y= 5;
+
+
+// 2. Type Error
+
+// let address = ;
+// console.log(address.length)
+
+const people = 5;
+// people();
+
+
+//  3. Reference Error
+
+function add(a, b) {
+    const result = a + b;
+    return result;
+}
+
+// console.log(result);
+
+if (true) {
+    // console.log(time)
+    const time = 10;
+}
+
+
+const person = {
+    name: 'sonibar'
+}
+
+person.map(a => console.log(a))
+
